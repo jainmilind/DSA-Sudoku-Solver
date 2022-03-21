5ece8e38069f7ff0d67789fdb27e4fbb3196c4ca
Tushar Chenan <tusky2002@gmail.com>
2022-03-21 16:13:56 +0530

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,8 +4,49 @@
 #include <QLabel>
 #include <QSpinBox>
 #include <QPushButton>
+#include <QDebug>
 
-int N = 3; // N X N sudoku grid
+using namespace std;
+
+constexpr int N = 4; // N X N sudoku grid
+
+vector<vector<QSpinBox*>> sudokuGrid;
+
+void initializeSudokuGrid(QGridLayout* layout) {
+    sudokuGrid.resize(N);
+    for(int i = 0; i < N; ++i) {
+        sudokuGrid[i].resize(N);
+        for(int j = 0; j < N; ++j) {
+            QSpinBox* box = new QSpinBox;
+            box->setMinimum(0);
+            box->setMaximum(N);
+            box->setSpecialValueText(" ");
+            box->setButtonSymbols(QAbstractSpinBox::NoButtons);
+            sudokuGrid[i][j] = box;
+            layout->addWidget(box, i, j);
+        }
+    }
+}
+
+vector<vector<int>> getSudokuGrid() {
+    vector<vector<int>> res;
+    res.resize(N);
+    for(int i = 0; i < N; ++i) {
+        res[i].resize(N);
+        for(int j = 0; j < N; ++j) {
+            res[i][j] = sudokuGrid[i][j]->value();
+        }
+    }
+    return res;
+}
+
+void generateRandomSudoku() {
+    qDebug() << QString("Generating random sudoku!");
+}
+
+bool isSudokuGridValid(const vector<vector<int>> &grid) {
+    return false;
+}
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -14,6 +55,7 @@ MainWindow::MainWindow(QWidget *parent)
         QDesktopWidget *temp = new QDesktopWidget();
         this->setFixedSize(temp->width()*0.5, temp->height()*0.5);
         delete temp;
+        this->setWindowTitle(QString::fromStdString("A " + to_string(N) + " X " + to_string(N) + " Sudoku Solver"));
     }
 
     QWidget *container = new QWidget(this);
@@ -21,41 +63,26 @@ MainWindow::MainWindow(QWidget *parent)
 
     QGridLayout *layout = new QGridLayout;
 
-//    QLabel *promptN = new QLabel(QString("Set the grid size"));
-//    layout->addWidget(promptN, 0, 0, 1, N, Qt::AlignHCenter);
-
-//    QSpinBox *getN = new QSpinBox;
-//    layout->addWidget(getN, 1, 0, 1, N, Qt::AlignHCenter);
-
-    // this is to change the grid value based on N
-
-    N = 9;
-
-    QLabel *mainText = new QLabel(QString("A 3X3 Sudoku Solver"));
-    layout->addWidget(mainText, 2, 0, 1, N, Qt::AlignHCenter);
-
-    std::vector<QSpinBox*> sudokuGrid;
-    sudokuGrid.reserve(N*N);
-
-    for(int i = 0; i < N; ++i) {
-        for(int j = 0; j < N; ++j) {
-            QSpinBox* box = new QSpinBox;
-            box->setMinimum(0);
-            box->setMaximum(N);
-            box->setSpecialValueText(" ");
-            sudokuGrid.push_back(box);
-            layout->addWidget(box, i+3, j);
-        }
-    }
+    initializeSudokuGrid(layout);
 
     QPushButton *generateSudoku = new QPushButton("Generate Sudoku");
-    layout->addWidget(generateSudoku, N+3, 0, 1, N, Qt::AlignHCenter);
+    layout->addWidget(generateSudoku, N+1, 0, 1, N, Qt::AlignHCenter);
+    QObject::connect(generateSudoku, &QPushButton::clicked, [&](){
+        generateRandomSudoku();
+    });
 
     QPushButton *verifySudoku = new QPushButton("Verify Sudoku");
-    layout->addWidget(verifySudoku, N+4, 0, 1, N, Qt::AlignHCenter);
+    layout->addWidget(verifySudoku, N+2, 0, 1, N, Qt::AlignHCenter);
+    QObject::connect(verifySudoku, &QPushButton::clicked, [&](){
+        vector<vector<int>> grid = getSudokuGrid();
+        qDebug() << isSudokuGridValid(grid);
+    });
 
     QPushButton *solveSudoku = new QPushButton("Solve Sudoku");
-    layout->addWidget(solveSudoku, N+5, 0, 1, N, Qt::AlignHCenter);
+    layout->addWidget(solveSudoku, N+3, 0, 1, N, Qt::AlignHCenter);
+    QObject::connect(solveSudoku, &QPushButton::clicked, [&](){
+        qDebug() << QString("Solve the sudoku!");
+    });
 
     container->setLayout(layout);
 }
