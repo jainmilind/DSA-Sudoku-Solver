ea3477721c963087d908f076b688df99087c555a
Tushar <tusky2002@gmail.com>
2022-04-18 22:43:58 +0530

    Formatted files

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,24 +1,27 @@
 #include "mainwindow.h"
+
+#include <QDebug>
 #include <QGridLayout>
 #include <QLabel>
-#include <QSpinBox>
-#include <QPushButton>
-#include <QDebug>
 #include <QMessageBox>
+#include <QPushButton>
+#include <QSpinBox>
+
 #include "sudoku.cpp"
+
 using namespace std;
 
 constexpr int WINDOW_L = sqrt_N * 150;
 constexpr int WINDOW_W = sqrt_N * 300;
 
-vector<vector<QSpinBox*>> sudokuGrid;
+vector<vector<QSpinBox *>> sudokuGrid;
 
-void initializeSudokuGrid(QGridLayout* layout) {
+void initializeSudokuGrid(QGridLayout *layout) {
     sudokuGrid.resize(N);
     for (int i = 0; i < N; ++i) {
         sudokuGrid[i].resize(N);
         for (int j = 0; j < N; ++j) {
-            QSpinBox* box = new QSpinBox;
+            QSpinBox *box = new QSpinBox;
             box->setMinimum(0);
             box->setMaximum(N);
             box->setSpecialValueText(" ");
@@ -41,7 +44,7 @@ vector<vector<int>> getSudokuGrid() {
     return res;
 }
 
-void setSudokuGrid(const vector<vector<int>>& grid) {
+void setSudokuGrid(const vector<vector<int>> &grid) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             sudokuGrid[i][j]->setValue(grid[i][j]);
@@ -57,10 +60,8 @@ void clearSudokuGrid() {
     }
 }
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{
-    this->setFixedSize(WINDOW_L*0.75, WINDOW_W*0.75);
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+    this->setFixedSize(WINDOW_L * 0.75, WINDOW_W * 0.75);
     this->setWindowTitle(QString::fromStdString("A " + to_string(N) + " X " + to_string(N) + " Sudoku Solver"));
 
     QWidget *container = new QWidget(this);
@@ -73,53 +74,46 @@ MainWindow::MainWindow(QWidget *parent)
     initializeSudokuGrid(layout);
 
     QPushButton *verifySudoku = new QPushButton("Verify Sudoku");
-    layout->addWidget(verifySudoku, N+1, 0, 1, N, Qt::AlignHCenter);
-    QObject::connect(verifySudoku, &QPushButton::clicked, [&](){
+    layout->addWidget(verifySudoku, N + 1, 0, 1, N, Qt::AlignHCenter);
+    QObject::connect(verifySudoku, &QPushButton::clicked, [&]() {
         vector<vector<int>> grid = getSudokuGrid();
         SUDOKU_STATUS valid = getSudokuStatus(grid);
         QMessageBox *Msgbox = new QMessageBox;
-        if(valid == INVALID) {
+        if (valid == INVALID) {
             Msgbox->setText("The Sudoku is invalid!");
-        }
-        else if(valid == COMPLETED) {
+        } else if (valid == COMPLETED) {
             Msgbox->setText("Congratulations! The Sudoku is completed!");
-        }
-        else {
+        } else {
             Msgbox->setText("The Sudoku is valid!");
         }
         Msgbox->exec();
     });
 
     QPushButton *solveSudoku = new QPushButton("Solve Sudoku");
-    layout->addWidget(solveSudoku, N+2, 0, 1, N, Qt::AlignHCenter);
-    QObject::connect(solveSudoku, &QPushButton::clicked, [&](){
-
+    layout->addWidget(solveSudoku, N + 2, 0, 1, N, Qt::AlignHCenter);
+    QObject::connect(solveSudoku, &QPushButton::clicked, [&]() {
         vector<vector<int>> grid = getSudokuGrid();
 
         if (solveSudokuGrid(grid))
-           setSudokuGrid(grid);
+            setSudokuGrid(grid);
         else
             qDebug() << QString("LMAO DED");
-
     });
 
     QPushButton *generateSudoku = new QPushButton("Generate Sudoku");
-    layout->addWidget(generateSudoku, N+3, 0, 1, N, Qt::AlignHCenter);
-    QObject::connect(generateSudoku, &QPushButton::clicked, [&](){
+    layout->addWidget(generateSudoku, N + 3, 0, 1, N, Qt::AlignHCenter);
+    QObject::connect(generateSudoku, &QPushButton::clicked, [&]() {
         clearSudokuGrid();
         setSudokuGrid(generateRandomSudoku());
     });
 
     QPushButton *clearSudoku = new QPushButton("Clear Sudoku");
-    layout->addWidget(clearSudoku, N+4, 0, 1, N, Qt::AlignHCenter);
-    QObject::connect(clearSudoku, &QPushButton::clicked, [&](){
+    layout->addWidget(clearSudoku, N + 4, 0, 1, N, Qt::AlignHCenter);
+    QObject::connect(clearSudoku, &QPushButton::clicked, [&]() {
         clearSudokuGrid();
     });
 
     container->setLayout(layout);
 }
 
-MainWindow::~MainWindow()
-{
-
-}
+MainWindow::~MainWindow() {}
--- sudoku.cpp
+++ sudoku.cpp
@@ -2,60 +2,55 @@
 using namespace std;
 
 constexpr int sqrt_N = 3;
-constexpr int N = sqrt_N * sqrt_N; // N X N sudoku grid
+constexpr int N = sqrt_N * sqrt_N;    // N X N sudoku grid
 
-enum SUDOKU_STATUS {INVALID, PARTIALLY_FILLED, COMPLETED};
+enum SUDOKU_STATUS { INVALID, PARTIALLY_FILLED, COMPLETED };
 
 bool canPlace(const vector<vector<int>> &grid, int val, int x, int y) {
-    if(grid[x][y] != 0) {
+    if (grid[x][y] != 0) {
         return false;
     }
     // iterate in column
-    for(int i = 0; i < N ; ++i) {
-        if(grid[i][y] == val)
-            return false;
+    for (int i = 0; i < N; ++i) {
+        if (grid[i][y] == val) return false;
     }
     // iterate in row
-    for(int j = 0; j < N ; ++j) {
-        if(grid[x][j] == val)
-            return false;
+    for (int j = 0; j < N; ++j) {
+        if (grid[x][j] == val) return false;
     }
     // iterate in box
-    int box_top_x = (x/sqrt_N)*sqrt_N;
-    int box_top_y = (y/sqrt_N)*sqrt_N;
-    for(int i = box_top_x; i < box_top_x + sqrt_N; ++i) {
-        for(int j = box_top_y; j < box_top_y + sqrt_N; ++j) {
-            if(grid[i][j] == val)
-                return false;
+    int box_top_x = (x / sqrt_N) * sqrt_N;
+    int box_top_y = (y / sqrt_N) * sqrt_N;
+    for (int i = box_top_x; i < box_top_x + sqrt_N; ++i) {
+        for (int j = box_top_y; j < box_top_y + sqrt_N; ++j) {
+            if (grid[i][j] == val) return false;
         }
     }
     return true;
 }
 
 SUDOKU_STATUS getSudokuStatus(const vector<vector<int>> &grid) {
-    int empty_cell_cnt = 0;
+    bool empty_cell = false;
     for (int i = 0; i < N; ++i) {
         unordered_set<int> cur;
-        for (int j = 0; j < N; j++) {
+        for (int j = 0; j < N; ++j) {
             if (grid[i][j] == 0) {
-                ++empty_cell_cnt;
+                empty_cell = true;
                 continue;
             }
-            if (cur.count(grid[i][j]))
-                return INVALID;
+            if (cur.count(grid[i][j])) return INVALID;
             cur.insert(grid[i][j]);
         }
     }
     // for each column
     for (int i = 0; i < N; ++i) {
         unordered_set<int> cur;
-        for (int j = 0; j < N; j++) {
+        for (int j = 0; j < N; ++j) {
             if (grid[j][i] == 0) {
-                ++empty_cell_cnt;
+                empty_cell = true;
                 continue;
             }
-            if (cur.count(grid[j][i]))
-                return INVALID;
+            if (cur.count(grid[j][i])) return INVALID;
             cur.insert(grid[j][i]);
         }
     }
@@ -66,39 +61,33 @@ SUDOKU_STATUS getSudokuStatus(const vector<vector<int>> &grid) {
             for (int k = j; k < sqrt_N + j; ++k) {
                 for (int l = i; l < sqrt_N + i; ++l) {
                     if (grid[k][l] == 0) {
-                        ++empty_cell_cnt;
+                        empty_cell = true;
                         continue;
                     }
-                    if (cur.count(grid[k][l]))
-                        return INVALID;
+                    if (cur.count(grid[k][l])) return INVALID;
                     cur.insert(grid[k][l]);
                 }
             }
         }
     }
-    if(empty_cell_cnt == 0) {
+    if (!empty_cell) {
         return COMPLETED;
-    }
-    else {
+    } else {
         return PARTIALLY_FILLED;
     }
 }
 
 bool solveSudokuGrid(vector<vector<int>> &grid, int x = 0, int y = 0) {
-    if (x == N)
-        return true;
-    if (y == N)
-        return solveSudokuGrid(grid, x + 1, 0);
-    if (grid[x][y] != 0)
-        return solveSudokuGrid(grid, x, y + 1);
+    if (x == N) return true;
+    if (y == N) return solveSudokuGrid(grid, x + 1, 0);
+    if (grid[x][y] != 0) return solveSudokuGrid(grid, x, y + 1);
     for (int i = 1; i <= N; ++i) {
-        if(canPlace(grid, i, x, y)) {
+        if (canPlace(grid, i, x, y)) {
             grid[x][y] = i;
-            bool nxt = solveSudokuGrid(grid, x, y+1);
-            if(nxt) {
+            bool nxt = solveSudokuGrid(grid, x, y + 1);
+            if (nxt) {
                 return nxt;
-            }
-            else {
+            } else {
                 grid[x][y] = 0;
             }
         }
@@ -115,7 +104,7 @@ vector<vector<int>> generateRandomSudoku() {
     vector<int> pos(N * N);
     iota(pos.begin(), pos.end(), 0);
     shuffle(pos.begin(), pos.end(), rng);
-    for (int i = 0; i < N * (N - 2); i++) {
+    for (int i = 0; i < N * (N - 2); ++i) {
         int x = pos[i];
         grid[x / N][x % N] = 0;
     }
